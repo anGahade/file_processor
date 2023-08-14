c91bd1462787828e4a4a023b02cbb1fc982c6bec
Gahade <angrygahade@gmail.com>
2023-08-14 12:48:44 +0200

    Add new function compare_files

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 """
-Реалізуйте програму, яка копіює вміст одного файлу в інший.
+Напишіть програму, яка відкриває два файли для читання та порівнює їх вміст,
+виводячи рядки, які є у першому файлі, але відсутні у другому.
 """
-source_file = 'test.txt'
-dest_file = 'test_2.txt'
 
 
-def copy_file():
-    with open(source_file, 'r') as src:
-        with open(dest_file, 'w') as dst:
-            dst.write(src.read())
+def compare_files(file1_path, file2_path):
+    try:
+        with open(file1_path, 'r', encoding='utf-8') as file1, open(file2_path, 'r', encoding='utf-8') as file2:
+            lines_file1 = set(file1.readlines())
+            lines_file2 = set(file2.readlines())
+
+            missing_lines = lines_file1 - lines_file2
+
+            print("Рядки, які є у першому файлі, але відсутні у другому:")
+            for line in missing_lines:
+                print(line.strip())
+    except FileNotFoundError:
+        print("Один з файлів не знайдено.")
 
 
-copy_file()
-
-print('Файл успішно скопійовано!')
-
+compare_files('test.txt', 'test_2.txt')
